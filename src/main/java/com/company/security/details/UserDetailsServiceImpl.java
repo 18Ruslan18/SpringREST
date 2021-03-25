5484bd754b076dbe3f1c9c02f4b287d2f24e986a
18Ruslan18 <79464524+18Ruslan18@users.noreply.github.com>
2021-03-25 15:36:31 +0300

    security

---
+++ src/main/java/com/company/security/details/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.company.security.details;
+import com.company.repositories.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+//        Optional<User> userCandidate = usersRepository.findOneByLogin(login);
+//        if (userCandidate.isPresent()) {
+//            return new UserDetailsImpl(userCandidate.get());
+//        } else throw new IllegalArgumentException("User not found");
+        return new
+                UserDetailsImpl(usersRepository.findOneByLogin(login)
+                .orElseThrow(IllegalArgumentException::new));
+    }
+}
